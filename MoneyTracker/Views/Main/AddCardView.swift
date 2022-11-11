42021b0a11a932fa605ba56d35981b1b0a11a750
KorobskoyRoman <shopone023@gmail.com>
2022-11-11 21:30:05 +0300

    feat: Configure add card view

---
+++ MoneyTracker/Views/Main/AddCardView.swift
@@ -0,0 +1,20 @@
+//
+//  AddCardView.swift
+//  MoneyTracker
+//
+//  Created by Roman Korobskoy on 11.11.2022.
+//
+
+import SwiftUI
+
+struct AddCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCardView()
+    }
+}
