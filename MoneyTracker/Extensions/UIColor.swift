65f03c8144f65127065430c56046756147a5bac0
KorobskoyRoman <shopone023@gmail.com>
2022-11-16 21:40:02 +0300

    feat: Update extensions

--- MoneyTracker/Extensions/UIColor.swift
+++ MoneyTracker/Extensions/UIColor.swift
@@ -5,4 +5,16 @@
 //  Created by Roman Korobskoy on 16.11.2022.
 //
 
-import Foundation
+import SwiftUI
+
+extension UIColor {
+    class func color(data: Data) -> UIColor? {
+        return try?
+        NSKeyedUnarchiver.unarchivedObject(ofClass: self, from: data) as? UIColor
+    }
+
+    func encode() -> Data? {
+        return try? NSKeyedArchiver.archivedData(withRootObject: self,
+                                                 requiringSecureCoding: false)
+    }
+}
