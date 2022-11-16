a75d19afd9e0169d8058371e752cc1a873083e27
KorobskoyRoman <shopone023@gmail.com>
2022-11-16 21:40:44 +0300

    feat: Add save card methods

--- MoneyTracker/Modules/Main/MainViewModel.swift
+++ MoneyTracker/Modules/Main/MainViewModel.swift
@@ -10,6 +10,15 @@ import SwiftUI
 protocol MainViewModelType {
     func addItem()
     func deleteAllItems(_ cards: FetchedResults<Card>)
+    func saveItem(name: String,
+                  number: String,
+                  limit: String,
+                  type: String,
+                  month: Int,
+                  year: Int,
+                  timestamp: Date,
+                  balance: String,
+                  color: Color)
 }
 
 final class MainViewModel: MainViewModelType  {
@@ -22,5 +31,25 @@ final class MainViewModel: MainViewModelType  {
 
     func deleteAllItems(_ cards: FetchedResults<Card>) {
         coreDataService.deleteAllItems(cards)
+    }
+
+    func saveItem(name: String,
+                  number: String,
+                  limit: String,
+                  type: String,
+                  month: Int,
+                  year: Int,
+                  timestamp: Date,
+                  balance: String,
+                  color: Color) {
+        coreDataService.saveItem(name: name,
+                                 number: number,
+                                 limit: limit,
+                                 type: type,
+                                 month: month,
+                                 year: year,
+                                 timestamp: timestamp,
+                                 balance: balance,
+                                 color: color)
     }
 }
--- MoneyTracker/Services/CoreData/CoreDataService.swift
+++ MoneyTracker/Services/CoreData/CoreDataService.swift
@@ -9,11 +9,38 @@ import CoreData
 import SwiftUI
 
 final class CoreDataService: ObservableObject {
-    let viewContext = PersistenceController.shared.container.viewContext
+    private let viewContext = PersistenceController.shared.container.viewContext
 
     func addItem() {
         let card = Card(context: viewContext)
         card.timestamp = Date()
+
+        do {
+            try viewContext.save()
+        } catch {
+            print(error)
+        }
+    }
+
+    func saveItem(name: String,
+                  number: String,
+                  limit: String,
+                  type: String,
+                  month: Int,
+                  year: Int,
+                  timestamp: Date,
+                  balance: String,
+                  color: Color) {
+        let card = Card(context: viewContext)
+
+        card.name = name
+        card.number = number
+        card.limit = Int32(limit) ?? 0
+        card.expMonth = Int16(month)
+        card.expYear = Int16(year)
+        card.timestamp = timestamp
+        card.balance = Int64(balance) ?? 0
+        card.color = UIColor(color).encode()
 
         do {
             try viewContext.save()
