007a21de12de9a71c98fb131a0a672862dd8d601
KorobskoyRoman <shopone023@gmail.com>
2022-11-16 21:39:44 +0300

    feat: Add extensions for string + UIColor

---
+++ MoneyTracker/Extensions/String+.swift
@@ -0,0 +1,8 @@
+//
+//  String+.swift
+//  MoneyTracker
+//
+//  Created by Roman Korobskoy on 16.11.2022.
+//
+
+import Foundation
---
+++ MoneyTracker/Extensions/UIColor.swift
@@ -0,0 +1,8 @@
+//
+//  UIColor.swift
+//  MoneyTracker
+//
+//  Created by Roman Korobskoy on 16.11.2022.
+//
+
+import Foundation
