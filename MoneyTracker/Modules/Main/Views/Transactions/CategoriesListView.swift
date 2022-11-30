db280e56a8e80dfbd5bcf44d3677dfd98399458e
KorobskoyRoman <shopone023@gmail.com>
2022-11-30 22:46:10 +0300

    fix: Arch refactoring

---
+++ MoneyTracker/Modules/Main/Views/Transactions/CategoriesListView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoriesListView.swift
+//  MoneyTracker
+//
+//  Created by Roman Korobskoy on 30.11.2022.
+//
+
+import SwiftUI
+
+struct CategoriesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoriesListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoriesListView()
+    }
+}
