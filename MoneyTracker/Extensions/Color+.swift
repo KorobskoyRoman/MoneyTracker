a700764496b5d82aca7d2a03f5d2c8ef70dc5e97
KorobskoyRoman <shopone023@gmail.com>
2022-12-09 19:54:59 +0300

    feat: New extension for colors

---
+++ MoneyTracker/Extensions/Color+.swift
@@ -0,0 +1,14 @@
+//
+//  Color+.swift
+//  MoneyTracker
+//
+//  Created by Roman Korobskoy on 09.12.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let cardTransactionBackground = Color("cardTransactionBackground")
+    static let bwText = Color("bwText")
+    static let bwBackground = Color("bwBackground")
+}
